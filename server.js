e5171cb1a4732d6b868355756dded0eaf06df32c
MaciejMl <szypki@gmail.com>
2023-06-03 22:46:59 +0200

    Add socket/socket-client to server/client&actions for server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const socket = require('socket.io');
 
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -8,6 +9,30 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Srever started on port: ', 8000);
+});
+
+const io = socket(server);
+
+let tasks = [];
+
+io.on('connection', (socket) => {
+  console.log(`Getting data for socket id: ${socket.id}`);
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (data) => {
+    tasks.push({ task: data.task, id: data.id });
+    console.log('Task added.');
+    socket.broadcast.emit('addTask', data);
+  });
+
+  socket.on('removeTask', (taskId) => {
+    const taskIdIndex = tasks.findIndex((task) => task.id === taskId);
+
+    if (taskIdIndex !== -1) {
+      const removedTask = tasks.splice(taskIdIndex, 1);
+      socket.broadcast.emit('removeTask', removedTask[0]);
+    }
+  });
 });
